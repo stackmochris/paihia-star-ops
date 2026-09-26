5837aae62927d84939d46681cfa075bac5ca4648
stackmochris <chris@stackmosaic.co.nz>
2026-09-26 13:18:00 +1200

    Wire Apps Script web app URL into config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// After you deploy Apps Script (see README), paste the Web App URL here.
 // Sheet: https://docs.google.com/spreadsheets/d/1uEPMq9wCpCeJoga3EOTyyBKsceV9rWNxBvd8Z1UkPeU
 window.STAR_OPS = {
   SHEET_ID: '1uEPMq9wCpCeJoga3EOTyyBKsceV9rWNxBvd8Z1UkPeU',
-  SCRIPT_URL: '',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwf1vIva6uawvUu3amCiEa8Y9muzfdkUojRF0oyTAy9-_YVN77LQ8cyyxHdZM-x919C/exec',
   csv: function (sheetName) {
     return 'https://docs.google.com/spreadsheets/d/' + this.SHEET_ID +
       '/gviz/tq?tqx=out:csv&sheet=' + encodeURIComponent(sheetName);
